6fa0db78b5f0a270b87eb83b334ce69564a96dad
bansht <natsun974@gmail.com>
2025-11-10 18:32:31 +0800

    update

--- server.js
+++ server.js
@@ -68,7 +68,9 @@ app.post("/subscribe", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
